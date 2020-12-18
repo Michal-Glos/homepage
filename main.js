5d17057f020ede82738475b3e90a1f7828ac1a38
Michał Głos <kornik90@poczta.fm>
2020-12-18 11:03:10 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+const firstName = 'Michał';
+const age = 30;
+const workTime = 2
+
+console.log(firstName);
+console.log(age)
+console.log(`Nazywam się ${firstName} i mam ${age} lat.`);
+console.log(`Cześć, miło Cię pzonać, jestem ${firstName} i mam ${age} lat.
+Moim głównym zainteresowaniem to fotografia którą zajmuję się od ${workTime} lat.`)
