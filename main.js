1bf47c29139d821565a910cbef0bb14b068cea93
Michał Głos <kornik90@poczta.fm>
2020-12-30 20:57:16 +0100

    reorganized to BEM class style

--- main.js
+++ main.js
@@ -7,3 +7,7 @@ console.log(age)
 console.log(`Nazywam się ${firstName} i mam ${age} lat.`);
 console.log(`Cześć, miło Cię pzonać, jestem ${firstName} i mam ${age} lat.
 Moim głównym zainteresowaniem to fotografia którą zajmuję się od ${workTime} lat.`)
+
+const heading = document.querySelector('.main-heading--js');
+
+heading.innerHTML = `Nazywam się ${firstName} i mam ${age} lat.`
