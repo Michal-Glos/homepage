b626def830adf5fb474a06b6b32a963274a27b84
Michał Głos <kornik90@poczta.fm>
2021-03-03 15:49:45 +0100

    JS learning

--- main.js
+++ main.js
@@ -11,3 +11,29 @@ Moim głównym zainteresowaniem to fotografia którą zajmuję się od ${workTim
 const heading = document.querySelector('.main-heading--js');
 
 heading.innerHTML = `Nazywam się ${firstName} i mam ${age} lat.`
+
+if (2 == '2') {
+    console.log('Java to nie Javascript')
+
+
+}
+
+const myNumber = 11;
+switch (myNumber) {
+    case 7:
+        console.log('Jestem siódemką');
+    break;
+    case 9:
+        console.log('Jestem dziewiątką');
+    break;
+    default:
+        console.log('Jestem czymś innym');
+}
+
+if (32>20) {
+    console.log('To prawda')        
+} else {
+    console.log('To nie prawda')
+}
+
+(32>20) ? console.log('to prawda'): console.log('to nie prawda');
